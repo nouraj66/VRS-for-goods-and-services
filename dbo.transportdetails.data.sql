eee80f15fc541bf0f699565f760bbc117dc45488
nouraj66 <nourajadhikari22@gmail.com>
2024-10-24 23:12:20 +0600

    Update dbo.transportdetails.data.sql

--- dbo.transportdetails.data.sql
+++ dbo.transportdetails.data.sql
@@ -1,4 +1,4 @@
 ﻿SET IDENTITY_INSERT [dbo].[transportdetails] ON
 INSERT INTO dbo.TransportDetails (Name, Phone, Address, Email, VehicleType, LoadType, AvailableDate)
-VALUES ('John Doe', '1234567890', '123 Main St', 'john.doe@example.com', 'Heavy', 'Soft Goods', '2024-10-16');
+VALUES ('nouraj', '1234567890', '123 Main St', 'nouraj@gmail.com', 'Heavy', 'Soft Goods', '2024-10-16');
 SET IDENTITY_INSERT TransportDetails ON;
